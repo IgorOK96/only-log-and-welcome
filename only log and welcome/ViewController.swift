bae55182cf2e9d44f43c7f0d86e723b9910aeb9f
IgorOK96 <IgorOK96@users.noreply.github.com>
2024-09-09 16:43:52 +0200

    Initial Commit

---
+++ only log and welcome/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  only log and welcome
+//
+//  Created by user246073 on 9/9/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
